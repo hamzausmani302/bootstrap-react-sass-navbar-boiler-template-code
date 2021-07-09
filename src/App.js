2de5ac8f844211dd0a6f6efd61c2846c90796480
hamzausmani302 <hamzausmani021@gmail.com>
2021-07-09 19:56:09 +0500

    bootstrap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './assets/css/style.css';
+
+import Navbar from './ReactComponents/Navbar.js';
+function App() {
+  return (
+    <div className="container-fluid" >
+          <Navbar />
+    </div>
+  );
+}
+
+export default App;
